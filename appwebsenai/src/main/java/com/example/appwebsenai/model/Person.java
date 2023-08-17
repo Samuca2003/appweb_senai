65d890a9cb2b35d4ba7d098626a27f194cfdda7f
Samuel <samuelborges1503@gmail.com>
2023-08-17 20:49:21 -0300

    #Adicionar página para cadastrar uma conta

--- appwebsenai/src/main/java/com/example/appwebsenai/model/Person.java
+++ appwebsenai/src/main/java/com/example/appwebsenai/model/Person.java
@@ -1,38 +1,48 @@
 package com.example.appwebsenai.model;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 public class Person {
 
     @Id
-     private Integer id;
+    private Integer id;
 
-     private String name;
+    private String name;
 
-     private String sexo;
+    private String sexo;
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
+    private ContaCorrentePF contaCorrentePF;
 
-     public String getSexo() {
+    public ContaCorrentePF getContaCorrentePF() {
+        return contaCorrentePF;
+    }
+
+    public void setContaCorrentePF(ContaCorrentePF contaCorrentePF) {
+        this.contaCorrentePF = contaCorrentePF;
+    }
+
+    public String getSexo() {
         return sexo;
     }
 
-     public void setSexo(String sexo) {
+    public void setSexo(String sexo) {
         this.sexo = sexo;
     }
 
-     public String getName() {
+    public String getName() {
         return name;
     }
 
-     public void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-     public Integer getId() {
+    public Integer getId() {
         return id;
     }
 
-     public void setId(Integer id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 }
